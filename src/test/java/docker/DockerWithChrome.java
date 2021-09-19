12b8ab9238185fc209c7cb9482765b88205a2edd
TaniyaFThomas <90641096+TaniyaFThomas@users.noreply.github.com>
2021-09-19 20:39:04 +0530

    Update DockerWithChrome.java

--- src/test/java/docker/DockerWithChrome.java
+++ src/test/java/docker/DockerWithChrome.java
@@ -5,24 +5,19 @@ import java.net.URL;
 
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxOptions;
-//import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class DockerWithChrome {
 
 	public static void main(String[] args)  throws MalformedURLException {
 		
-//		DesiredCapabilities dc = DesiredCapabilities.firefox();
-//		URL url = new URL("http://localhost:4444/wb/hub");
-//		RemoteWebDriver driver = new RemoteWebDriver(url, dc);
-		
-//		RemoteWebDriver driver = new RemoteWebDriver(
-//				 new URL("http://localhost:4444/wd/hub"),
-//				 new ChromeOptions());
-		
 		RemoteWebDriver driver = new RemoteWebDriver(
 				 new URL("http://localhost:4444/wd/hub"),
-				 new ChromeOptions());
+				 new ChromeOptions().addArguments("--no-sandbox",
+						 "start-maximized",
+						 "disable-infobars",
+						 "--disable-extensions",
+						 "--disable-dev-shm-usage"));
 		driver.get("https://opensource-demo.orangehrmlive.com/");
 		System.out.println("Title of the page:"+ driver.getTitle());
 		driver.quit();
