1352748f0db805b1d90dd1659f8f344af7884008
TaniyaFThomas <90641096+TaniyaFThomas@users.noreply.github.com>
2021-09-19 20:35:49 +0530

    Update ChromeTest1.java

--- src/test/java/docker/ChromeTest1.java
+++ src/test/java/docker/ChromeTest1.java
@@ -4,7 +4,6 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 import org.openqa.selenium.chrome.ChromeOptions;
-//import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
 
@@ -14,7 +13,11 @@ public class ChromeTest1 {
 		public void test1() throws MalformedURLException {
 			RemoteWebDriver driver = new RemoteWebDriver(
 					 new URL("http://localhost:4444/wd/hub"),
-					 new ChromeOptions());
+					 new ChromeOptions().addArguments("--no-sandbox",
+						 "start-maximized",
+						 "disable-infobars",
+						 "--disable-extensions",
+						 "--disable-dev-shm-usage"));
 			
 			driver.get("https://www.gmail.com/");
 			System.out.println("Title of the page:"+ driver.getTitle());
