e8d633d1dbb50d3cc9886fd1e299d00aaa120678
TaniyaFThomas <90641096+TaniyaFThomas@users.noreply.github.com>
2021-09-20 15:03:20 +0530

    Update ChromeTest1.java

--- src/test/java/docker/ChromeTest1.java
+++ src/test/java/docker/ChromeTest1.java
@@ -6,6 +6,7 @@ import java.net.URL;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.Test;
+import org.testng.Assert;
 
 public class ChromeTest1 {
 		
@@ -21,7 +22,7 @@ public class ChromeTest1 {
 			
 			driver.get("https://www.gmail.com/");
 			System.out.println("Title of gmail:"+ driver.getTitle());
-			assertEquals("Gmail", driver.getTitle());
+			Assert.assertEquals("Gmail", driver.getTitle());
 			driver.quit();
 			
 		}
