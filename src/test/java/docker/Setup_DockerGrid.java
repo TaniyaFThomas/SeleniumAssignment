dee30ab36eb1abae81846b5688ddd98ffd2eef3b
TaniyaFThomas <90641096+TaniyaFThomas@users.noreply.github.com>
2021-09-20 15:10:24 +0530

    Update Setup_DockerGrid.java

--- src/test/java/docker/Setup_DockerGrid.java
+++ src/test/java/docker/Setup_DockerGrid.java
@@ -11,14 +11,14 @@ public class Setup_DockerGrid {
 	@BeforeTest
 	void startDockerGrid() throws IOException, InterruptedException {
 		Runtime.getRuntime().exec("cmd /c start start_dockergrid.bat");
-		Thread.sleep(180000);
+		Thread.sleep(120000);
 	}
 	
 	@AfterTest
 	void stopDockerGrid() throws IOException, InterruptedException {
 		Runtime.getRuntime().exec("cmd /c start stop_dockergrid.bat");
 		Thread.sleep(60000);
-		Runtime.getRuntime().exec("taskkill /f /im cmd.exe"); //closes cmd
+		//Runtime.getRuntime().exec("taskkill /f /im cmd.exe"); //closes cmd
 	}
 
 
